2d03796711a7a7c475d2f9a14a70b5673c629042
wuteng <wuteng.wageco@foxmail.com>
2021-09-28 17:11:53 +0800

    1292 优化(计算前缀和) O(m*n)---time beat 97%

--- 1292.maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
+++ 1292.maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.cpp
@@ -13,9 +13,24 @@ class Solution {
     if (i + maxside > m || j + maxside > n) return false;
     return true;
   }
+
+  int squareSum(vector<vector<int>>& prefixSum, int x1, int y1, int x2,
+                int y2) {
+    return prefixSum[x2][y2] - prefixSum[x1][y2] -
+           prefixSum[x2][y1] + prefixSum[x1][y1];
+  }
+
   int maxSideLength(vector<vector<int>>& mat, int threshold) {
     int m = mat.size();
     int n = mat[0].size();
+    // 计算前缀和
+    vector<vector<int>> prefixSum(m + 1, vector<int>(n + 1));
+    for (int i = 1; i <= m; i++) {
+      for (int j = 1; j <= n; j++) {
+        prefixSum[i][j] = prefixSum[i - 1][j] + prefixSum[i][j - 1] -
+                          prefixSum[i - 1][j - 1] + mat[i - 1][j - 1];
+      }
+    }
     // 从1开始 每次尝试成功就+1,直到失败
     int maxside = 1;
     for (int i = 0; i < m; ++i) {
@@ -23,14 +38,9 @@ class Solution {
         // i,j 为一个正方形的左上方的点
         if (isSquare(m, n, i, j, maxside)) {
           // 计算正方形元素和
-          int tmp = 0;
-          for (int x = i; x < i + maxside; ++x) {
-            for (int y = j; y < j + maxside; ++y) {
-              tmp += mat[x][y];
-            }
-          }
+          int tmp = squareSum(prefixSum, i, j, i + maxside, j + maxside);
           if (tmp <= threshold) {
-            ++maxside; // +1
+            ++maxside;  // +1
           } else {
             ++j;
           }
