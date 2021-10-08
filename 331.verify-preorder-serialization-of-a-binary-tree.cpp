cd831c1cfc8dfb41cd0c4077ec56f21b01f17f01
wuteng <wuteng.wageco@foxmail.com>
2021-10-08 16:18:33 +0800

    331 regex "\d+,#,#"

--- 331.verify-preorder-serialization-of-a-binary-tree.cpp
+++ 331.verify-preorder-serialization-of-a-binary-tree.cpp
@@ -4,21 +4,17 @@
  * [331] Verify Preorder Serialization of a Binary Tree
  */
 #include <bits/stdc++.h>
+using std::regex;
 using std::string;
 using std::vector;
 // @lc code=start
 class Solution {
  public:
   bool isValidSerialization(string preorder) {
-    std::istringstream is(preorder);
-    string node;
-    int diff = 1;
-    while (std::getline(is, node, ',')) {
-      if (--diff < 0) return false;
-      if (node != "#") diff += 2;
-    }
-    return (diff == 0);
-    // The tree is full binary tree.
+    regex rg("\\d+,#,#");
+    string newpreorder = std::regex_replace(preorder, rg, "#");
+    return (newpreorder == "#") ||
+           (newpreorder != preorder && isValidSerialization(newpreorder));
   }
 };
 // @lc code=end
